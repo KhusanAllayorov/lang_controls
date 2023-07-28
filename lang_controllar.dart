9aac5c9fd7bac4b590590d75f6f6f9e75a919c43
Kxusan <husan4712@gmail.com>
2023-07-28 11:25:00 +0500

    Created Enums and Written function for enums

--- lang_controllar.dart
+++ lang_controllar.dart
@@ -1,5 +1,55 @@
 import 'dart:io';
 
 class LangControls {
+}
+enum UzCategory {
+  saladlar,
+  ichimliklar,
+  fast_food,
+  shirinliklar,
+  milliy_taomlar,
+}
 
+void Uzb() {
+  print("============================");
+  UzCategory.values.forEach((element) {
+    print("\t\t${element.name}");
+  });
+  print("============================");
+}
+
+enum RusCategory {
+  saladlar("салад"),
+  ichimliklar("напитки"),
+  fast_food("фаст фууд"),
+  shirinliklar("сладки"),
+  milliy_taomlar("натсинални еда");
+
+  final String type;
+
+  const RusCategory(this.type);
+}
+
+void Rus() {
+  print("===============================");
+  RusCategory.values.forEach((element) {
+    print("\t\t${element.type}");
+  });
+  print("===============================");
+}
+
+enum IngCategory {
+  salads,
+  drinks,
+  fast_food,
+  cokes,
+  notional_foods,
+}
+
+void Eng() {
+  print("===============================");
+  IngCategory.values.forEach((element) {
+    print("\t\t${element.name}");
+  });
+  print("===============================");
 }
